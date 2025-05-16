bb7011c2af0d6003a9910e26e9ddbe412b35a87f
Estrella-3010 <122043035@upq.edu.mx>
2025-05-16 15:30:46 -0600

    Función arrow function
    se convirtió una funcion tradicional a uno arrow function, permitiendo la simplicidad de código y facilidad de compilación

--- Practica 1/VariablesYFunciones.js
+++ Practica 1/VariablesYFunciones.js
@@ -7,4 +7,6 @@ nombre = "Ana Maria";
 let saludo = "hola, " + nombre + ". Tienes " + edad + " años.";
 console.log(saludo);
 
-
+// Ejercicio 2
+const cuadrado = (numero) => {return(numero * numero);}
+console.log("Resultado de la multiplicación: "+cuadrado(10));
