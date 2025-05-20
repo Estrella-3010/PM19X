13d26cc8ff5027ade1af51b0cfd929dc15e1254b
Estrella-3010 <122043035@upq.edu.mx>
2025-05-19 18:48:05 -0600

    Ejercicio b
    Se utilizó filter para poder filtrar entre todas las propiedades del objeto cual de los productos su precio es mayor a 1000, solo los productos que cumplan con esta condición se almacenaran en el nuevo arreglo llamado precios

---
+++ Practica2/filterYmap.js
@@ -0,0 +1,11 @@
+//ejercicio b
+
+const productos =[
+    {nombre:"Laptop", precio: 1200},
+    {nombre:"Mouse", precio: 250},
+    {nombre:"Teclado", precio: 750},
+    {nombre:"Monitor", precio: 3000},
+]
+
+const precios = productos.filter(productos => productos.precio > 1000)
+console.log(precios);
