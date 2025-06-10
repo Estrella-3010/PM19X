4276710c68eefffebc33068c2fa717958b7cbc18
Estrella-3010 <122043035@upq.edu.mx>
2025-06-10 15:38:12 -0600

    Creación de otro componente

--- myapp-b/App.js
+++ myapp-b/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+    const Texto = ()=>{
+      return(
+        <Text>Hola mundo</Text>
+      );
+    }
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto />
       <StatusBar style="auto" />
+      <Button title="obtener"/>
     </View>
   );
 }
