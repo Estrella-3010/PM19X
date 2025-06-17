cef77bc3013cb52452711d6fde44f19684e3e020
Estrella-3010 <122043035@upq.edu.mx>
2025-06-17 14:44:18 -0600

    Styles

--- myapp-b/App.js
+++ myapp-b/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';  
+import React,{useState} from 'react';
 
-    const Texto = (props)=>{
-      const {children} = props;
+    const Texto = ({style})=>{
+      const [contenido, setContenido] = useState('hola mundo');
+      actualizartexto=()=>{setContenido('state modificado')};
       return(
-        <Text>{children}</Text>
+        <Text style={[styles.text, style]} onPress={actualizartexto}>
+    
+          {contenido}
+        </Text>
+      );
+    }
+
+    const Boton = ()=>{
+      const [title, setTitle] = useState('presiona');
+      actualizarTitulo=()=>{setTitle('titulo modificado')};
+      return(
+        <Button title={title} onPress={actualizarTitulo}></Button>
+
       );
     }
 
@@ -12,11 +26,11 @@ export default function App() {
   return (
 
     <View style={styles.container}>
-      <Texto>Hola con children</Texto>
-      <Texto>mundo</Texto>
+      <Texto style={styles.rojo}></Texto>
+      <Texto style={styles.amarrillo}></Texto>
       <StatusBar style="auto" />
-      <Button title="obtener"/>
-      <Texto>react native</Texto>
+      <Boton/>
+      <Texto style={styles.verde}></Texto>
     </View>
   );
 }
@@ -27,5 +41,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection:'column',
+  },
+  text:{
+    color: 'black',
+    fontSize: 28,
+    width: 100,
+    height: 100,
+  },
+  rojo: {
+    backgroundColor: 'red',
+  },
+  amarrillo: {
+    backgroundColor: 'yellow',
+  },
+  verde: {
+    backgroundColor: 'green',
   },
 });
