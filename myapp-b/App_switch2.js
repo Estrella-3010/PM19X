7a985db147f6fc3b0d2ac6609996e03ff0e1bce1
Estrella-3010 <122043035@upq.edu.mx>
2025-06-20 22:40:58 -0600

    Switch

---
+++ myapp-b/App_switch2.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { View, Switch, StyleSheet, Text } from "react-native";
+
+const App = () => {
+  const [activo, setActivo] = useState(false);
+
+  const cambiarSwitch = () => {
+    setActivo((previousState) => !previousState);
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Activar Característica:</Text>
+
+      <Switch onValueChange={cambiarSwitch} value={activo} />
+
+      <Text style={styles.statusText}>
+        Estado actual: {activo ? "Activado" : "Desactivado"}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  statusText: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "#666",
+  },
+});
+
+export default App;
