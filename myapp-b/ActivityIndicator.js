b2035e773f5589ebfc1ca915068e7f35fb43b834
Estrella-3010 <122043035@upq.edu.mx>
2025-06-24 15:36:40 -0600

    ActivityIndicator

---
+++ myapp-b/ActivityIndicator.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, ActivityIndicator } from "react-native";
+
+export default function App (){
+    const [cargando, setCargando] = useState(false);
+    const [datos, setDatos] =useState('');
+
+    const SimularCarga = () =>{
+        setCargando(true);
+        setDatos('');
+
+        setTimeout(()=>{
+            setCargando(false);
+            setDatos('Datos cargados exitosamente');
+        }, 8000);
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Activity indicator</Text>
+            <Button title="Cargar datos" onPress={SimularCarga} color='#007AFF'/>
+            {
+                cargando &&(
+                    <ActivityIndicator size='large' color='#007AFF' style={styles.loader}/>
+                )
+            }
+            {datos !== '' && <Text>{datos}</Text>}
+            <StatusBar style="auto" />
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#f5f5f5',
+        alignItems:'center',
+        justifyContent:'center',
+        padding: 20,
+    },
+    titulo:{
+        fontSize: 24, 
+        fontWeight: 'bold',
+        marginBottom:20, 
+    },
+    loader:{
+        marginVirtical: 20,
+        
+    }
+
+
+});
